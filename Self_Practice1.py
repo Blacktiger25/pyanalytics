6276d05b0f90687ec74a2773f788b2c1c4568639
Blacktiger25 <63055591+Blacktiger25@users.noreply.github.com>
2020-04-07 01:45:19 +0530

    07/04/2020
    Self practice
    - Convert Celcius to Fahrenheit
    - Average of two numbers
    ** how to run two codes separately?
    **how to create partition?

---
+++ Self_Practice1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 01:08:28 2020
+
+@author: Udbhav
+"""
+# Convert Degree Celcius to Degree Fahreneit
+temp=  eval(input('Enter a temperature in Celcius: '))
+print('In Fahrenheit, that is', 9/5*temp+32)
+
+# Average of two numbers
+num1= eval(input('Enter the first number: '))
+num2= eval(input('Enter the Second number: '))
+print('The average of the numbers you entered is', (num1+num2)/2)
+
+
+87
